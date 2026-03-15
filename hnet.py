77d6cdebc48846e5995b9e1e1ca2eeff8f6def7f
Jatin Prakash <jatinprakash1511@gmail.com>
2026-03-15 13:05:22 -0400

    setup basic logic for compressor

--- hnet.py
+++ hnet.py
@@ -5,7 +5,39 @@ import torch.nn as nn
 class Compressor:
     # compressor "implicitly" decides how to chunk up the sequence
     # it acts as a router
-    pass
+    def __init__(self, config):
+        # TODO: fill in rest
+        pass
+
+    def chunk_and_compress(self, x):
+        # get probabilities to chunk at a particular position
+        # for now, let's just use a small MLP
+
+        # x: [B, L, D]
+
+        # do a pointwise MLP
+        probs = self.should_chunk_mlp(x) # [B, L, D] -> [B, L]
+
+        boundaries = torch.where(probs > self.threshold, dim=-1) # [B, K]
+
+        # now we need a way to extract tokens exactly at that position
+        # this is how we have decided to "compress" for now, just picking up representations at that position
+        compressed_x = x[boundaries] # [B, K, D] hopefully!
+
+        return compressed_x, boundaries, probs
+
+
+    def forward(self, x):
+        # x: [B, L, D]
+
+        # go through some layers
+        for layer in self.layers:
+            x = layer(x)
+
+        compressed_x, boundaries, probs = self.chunk_and_compress(x)
+
+        return x, compressed_x, boundaries
+        
 
 
 class Decoder:
