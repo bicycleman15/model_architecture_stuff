591a00d4d018a84458eb17e72859b7ca3fc31908
Jatin Prakash <jatinprakash1511@gmail.com>
2026-04-09 14:31:16 -0400

    update cmd

--- submit.sh
+++ submit.sh
@@ -37,6 +37,32 @@ transformer.n_layer=6 \
 transformer.dim=768 \
 transformer.n_head=12
 
+accelerate launch --config-file accelerate.yaml --mixed_precision=bf16 --num_processes=1 train.py \
+--config-path config/dynamic_chunk \
+--config-name char.yaml \
+wandb.project="tinystories-padded-mod15" \
+wandb.exp_name="uniform-8 6L lr 8e-4" \
+\
+train.batch_size=16 \
+train.global_batch_size=32 \
+\
+train.train_steps=4000 \
+eval.eval_interval=400 \
+eval.eval_iters=50 \
+\
+optimizer.lr=8e-4 \
+optimizer.min_lr=8e-5 \
+\
+model_type=hourglass \
+hourglass.block_size=8192 \
+hourglass.chunk_method="uniform" \
+hourglass.chunk_size=8 \
+\
+hourglass.dim=768 \
+hourglass.n_head=12 \
+hourglass.n_compressor_layers=3 \
+hourglass.n_processor_layers=6 \
+hourglass.n_decoder_layers=3
 
 
 echo "Run finished at: "
